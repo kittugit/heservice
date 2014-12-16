c88127cd243a0d2aa9f1064c117d33e5f4435655
Krishnamoorthy Damodaran <kittu_14@yahoo.com>
2014-12-15 23:59:15 -0600

    'gulp_added'

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var expenses = require('./routes/expenses');
 
 var app = express();
 
@@ -16,6 +17,13 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/expenses', expenses);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/expenses.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+  var expenses = [
+    // expense:  id, group_id, amount, date, description, category
+	 {id:0, date:'2014-12-12', category: 'Gas', amount: 23.78, description: 'Honda Car' } ,
+	 {id:1, date:'2014-12-1', category: 'Gas', amount: 43.78, description: 'Tour Car' } ,
+	 {id:2, date:'2014-12-1', category: 'Gas', amount: 43.78, description: '2nd Car' } ,
+	 {id:3, date:'2014-12-1', category: 'Groceries', amount: 76.00, description: '2nd Car' }
+  ];
+  
+  var expense_group = [ {id: 0, date: '2014-12-01', name: 'Dec 2014'}, 
+						{id: 1, date: '2014-11-01', name: 'Nov 2014'},
+						{id: 2, date: '2014-10-01', name: 'Oct 2014'},];
+
+						
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  res.json(expenses);
+});
+
+router.post('/', function(req, res) {
+	console.log('request body' + req.body);
+	console.log('request body' + req.body.id);
+	
+    expenses.push(req.body);
+	res.status(200).end();
+});
+
+router.get('/bycategory', function(req, res){
+	    
+		var byCategory = {};
+		var totalExpense = 0;
+		for(i in expenses){
+		  if(expenses[i].category){
+			if(byCategory[expenses[i].category]){
+			  byCategory[expenses[i].category] += expenses[i].amount;
+			}else{
+			  byCategory[expenses[i].category] = expenses[i].amount;
+			}   
+			totalExpense += expenses[i].amount;				
+			}
+		}
+		byCategory.Total = totalExpense;
+		console.log("groupByCategory" + byCategory);
+		
+		return byCategory;
+	});
+
+module.exports = router;
